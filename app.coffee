e9ddf15e8437a3fa6e3eebe0569aea080a0d0e90
jacoblwe20 <jacoblowe2.0@gmail.com>
2012-12-21 23:19:28 -0800

    adding mailchimp intergration

--- app.coffee
+++ app.coffee
@@ -6,6 +6,21 @@ members = require("./members.json")
 routes = require("./routes")
 http = require("http")
 path = require("path")
+MailChimpAPI = require('mailchimp').MailChimpAPI;
+
+apiKey = process.env.MAILCHIMPAPIKEY;
+
+#try mailchimp api
+
+try
+  api = new MailChimpAPI(apiKey,
+    version: "1.3"
+    secure: false
+  )
+catch error
+  console.log error.message
+
+
 app = express()
 
 app.configure ->
@@ -28,6 +43,22 @@ app.locals.members = members
 app.get "/", routes.index
 
 app.get "/locations", routes.locations
+
+app.post "/subscribe", (req, res) ->
+  email = req.query.email;
+
+  if(api)
+    #need to add in list id
+    api.listSubscribe({id: '', email: email}, function(err, _res){
+      if(err)
+        res.json(err);
+      else
+        res.json(_res);
+
+    })
+  else
+    #shot to mailchimp sign up page
+    res.redirect('')
 
 http.createServer(app).listen app.get("port"), ->
   console.log "Express server listening on port " + app.get("port")
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,53 @@
-/* Load in analytics 
- * ========================
- * First Analytics then Experiments
+/*
+ * Small Script to send Email to mailchimp
  */
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-37210378-1']);
-_gaq.push(['_trackPageview']);
 
-(function() {
-var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
+(function($){
 
+
+
+	var sendForm = function(form){
+
+		var inputs 	= form.find('input'),
+			method 	= form.attr('method'),
+			submit 	= form.find('input[type=submit]'),
+			url 	= form.attr('action')
+			compile = function(callback){
+				var obj = {};
+				inputs.each(function(){
+					var ele 	= $(this),
+						name 	= ele.attr('name'),
+						val 	= ele.val();
+
+					obj[name] = val;
+				});
+
+				callback(obj);
+			},
+			send = function(data, callback){
+				$.ajax({
+					url 	: url,
+					method 	: method,
+					data 	: data,
+					success : function(res){
+						callback(res);
+					}
+				});
+			};
+		console.log(submit);
+
+		submit.click(function(e){
+			compile(function(res){
+				send(res, function(_res){
+					console.log(res);
+				});
+			});
+			return false;
+		});
+	};
+
+	sendForm($('.email-form'));
+
+
+
+}(jQuery))
